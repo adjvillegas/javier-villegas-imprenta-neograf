145bb16cc49f9751b31c5024265cce8fbe2c5bdc
Javier Villegas <adj.villegas@gmail.com>
2021-06-07 12:51:45 -0300

    Commit Inicial

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <h1>Bienvenidos a Imprenta Neograf</h1>
+      </header>
+      <main className="App-main">
+          <p>Proyecto inicial usando CLI de ReactJs.</p>
+         
+          <p>Pagina en construcción para e-commerce de una Imprenta</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
