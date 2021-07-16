0319b7339855275f9e28c5d437fb896549254897
Javier Villegas <adj.villegas@gmail.com>
2021-07-16 14:34:56 -0300

    Fecha dinamica

--- src/components/Cart/CartHeader.js
+++ src/components/Cart/CartHeader.js
@@ -1,10 +1,25 @@
 import React from 'react'
 
 const CartHeader = () => {
+
+    const zeroLeft = (n) => {
+        return ("00" + n).slice(-2);
+    }
+
+    const getCurrentDay = () => {
+        const date = new Date()
+        
+        return [ zeroLeft(date.getDate()),
+                 zeroLeft(date.getMonth()+1),
+                 date.getFullYear()        
+        ].join('/')
+        
+    }
+
     return (
         <div className="row row-cols-4">
             <p className="col-2 text-end">Fecha:</p>
-            <p className="col-10">10/7/2021 13:54:40</p>
+            <p className="col-10">{getCurrentDay()}</p>
         </div>
     )
 }
